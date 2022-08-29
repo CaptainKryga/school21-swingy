952329c3cece7fe913d8484fb8d6e222b6eba727
CaptainKryga <krygadenis@gmail.com>
2022-08-29 18:44:47 +0200

    add check argv and output console log

--- src/main/java/org/jbashiri/Main.java
+++ src/main/java/org/jbashiri/Main.java
@@ -2,6 +2,10 @@ package org.jbashiri;
 
 public class Main {
     public static void main(String[] argv) {
-        System.out.println("Hello world!");
+        if (argv.length != 1 || (!argv[0].equals("gui") && !argv[0].equals("console"))) {
+            System.out.println("usage: java -jar swingy.jar <gui or console>");
+            return;
+        }
+        System.out.println("=============> Main");
     }
 }
