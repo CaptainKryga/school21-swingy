627f0cd29e9930dfa33f64f41d5640c726006f21
CaptainKryga <krygadenis@gmail.com>
2022-08-30 16:39:10 +0200

    upd utils

--- src/main/java/org/jbashiri/utils/CustomLogger.java
+++ src/main/java/org/jbashiri/utils/CustomLogger.java
@@ -1,2 +1,21 @@
-package org.jbashiri.utils;public class CustomLogger {
+package org.jbashiri.utils;
+
+public class CustomLogger {
+    public static CustomLogger singleton;
+
+    //3 мусор
+    //2 не очень важные
+    //1 важные
+    //0 ничего
+    private int lvl;
+    public CustomLogger(int lvl) {
+        singleton = this;
+        this.lvl = lvl;
+    }
+
+    public void printLog(String log, int lvl) {
+        if (lvl <= this.lvl) {
+            System.out.println("CustomLogger: " + log);
+        }
+    }
 }
--- src/main/java/org/jbashiri/utils/CustomRandom.java
+++ src/main/java/org/jbashiri/utils/CustomRandom.java
@@ -1,2 +1,9 @@
-package org.jbashiri.utils;public class CustomRandom {
+package org.jbashiri.utils;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class CustomRandom {
+    public static int getRandom(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min,max);
+    }
 }
