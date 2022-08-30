93a9a82667eb977191246f3f6e23b5856c711e4d
CaptainKryga <krygadenis@gmail.com>
2022-08-30 14:17:39 +0200

    add main

--- src/main/java/org/jbashiri/Main.java
+++ src/main/java/org/jbashiri/Main.java
@@ -1,11 +1,19 @@
 package org.jbashiri;
 
+import org.jbashiri.controller.ControllerInit;
+import org.jbashiri.view.init.UIInit;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] argv) {
         if (argv.length != 1 || (!argv[0].equals("gui") && !argv[0].equals("console"))) {
             System.out.println("usage: java -jar swingy.jar <gui or console>");
             return;
         }
+
+        new ControllerInit().switchUI(argv[0].equals("console"));
+
         System.out.println("=============> Main");
     }
 }
