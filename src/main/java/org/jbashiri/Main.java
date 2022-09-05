a5f7a47dafd1f29aeeb4150f40e63c9fefd44f23
CaptainKryga <krygadenis@gmail.com>
2022-09-05 11:31:04 +0200

    save main

--- src/main/java/org/jbashiri/Main.java
+++ src/main/java/org/jbashiri/Main.java
@@ -1,9 +1,10 @@
 package org.jbashiri;
 
 import org.jbashiri.controller.ControllerInit;
-import org.jbashiri.view.init.UIInit;
+import org.jbashiri.model.artifats.Artifact;
+import org.jbashiri.utils.CustomLogger;
 
-import java.util.Scanner;
+import static org.jbashiri.utils.CustomMath.getPow;
 
 public class Main {
     public static void main(String[] argv) {
@@ -12,8 +13,26 @@ public class Main {
             return;
         }
 
-        new ControllerInit().switchUI(argv[0].equals("console"));
+        boolean isConsole = argv[0].toLowerCase().equals("console");
 
-        System.out.println("=============> Main");
+        new CustomLogger(2);
+        new ControllerInit(isConsole);
+
+//        new Artifact(1,50);
+//        new Artifact(1,50);
+//        new Artifact(1,50);
+//        new Artifact(1,50);
+//        new Artifact(1,50);
+//        new Artifact(1,50);
+
+//        for (int i = 0; i < 10; i++) {
+//            int str = (i - 1) * 5 + 10;
+//            CustomLogger.singleton.printLog("(" + i + " * 1000 + getPow(" + i + " - 1, 2) * 450)", 3);
+//            CustomLogger.singleton.printLog("(" + i + "000 + " + getPow(i - 1, 2) + " * 450)", 3);
+//            CustomLogger.singleton.printLog("(" + i + "000 + " + getPow(i - 1, 2) * 450, 3);
+//            CustomLogger.singleton.printLog(str + "", 3);
+//        }
+
+
     }
 }
